cf358c41f7609973d5f2286bb316545b2fed8652
MacBook Pro <admin@Vlad.local>
2024-12-09 10:34:52 +0200

    правки сервисов

--- app/js/main.js
+++ app/js/main.js
@@ -1,191 +1,177 @@
-gsap.registerPlugin(ScrollTrigger) 
+gsap.registerPlugin(ScrollTrigger)
 
+items = document.querySelectorAll('.services-inner-item')
 
-items = document.querySelectorAll('.services-inner-item');
+items.forEach(function (item, index) {
+	item.addEventListener('mouseover', function () {
+		// const img = this.getAttribute('data-img');
+		const text = this.getAttribute('data-text')
 
+		// const mainImg = document.getElementById('img');
+		const mainText = document.getElementById('text')
 
-items.forEach(function(item, index) {
-	item.addEventListener("mouseover", function(){
-		const img = this.getAttribute('data-img');
-		const text = this.getAttribute('data-text');
-
-		const mainImg = document.getElementById('img');
-		const mainText = document.getElementById('text');
-
-		mainImg.setAttribute('src', img);
-		mainText.innerHTML = text;
-		mainText.style.opacity = '1';
-		mainImg.style.opacity = '1';
+		// mainImg.setAttribute('src', img);
+		mainText.innerHTML = text
+		mainText.style.opacity = '1'
+		mainImg.style.opacity = '1'
 	})
-	
-});
-
+})
 
 // Получаем ссылки меню и секции
-const menuLinks = document.querySelectorAll('.menu-link');
-const sections = document.querySelectorAll('.section');
+const menuLinks = document.querySelectorAll('.menu-link')
+const sections = document.querySelectorAll('.section')
 
 function updateActiveLink() {
-    const scrollPosition = window.scrollY;
+	const scrollPosition = window.scrollY
 
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        const sectionBottom = sectionTop + section.offsetHeight;
+	sections.forEach(section => {
+		const sectionTop = section.offsetTop
+		const sectionBottom = sectionTop + section.offsetHeight
 
-        if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-            const sectionId = '#' + section.getAttribute('id');
-            menuLinks.forEach(link => {
-                link.classList.toggle('active', link.getAttribute('href') === sectionId);
-            });
-        }
-    });
+		if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+			const sectionId = '#' + section.getAttribute('id')
+			menuLinks.forEach(link => {
+				link.classList.toggle('active', link.getAttribute('href') === sectionId)
+			})
+		}
+	})
 }
 
-window.addEventListener('scroll', updateActiveLink);
+window.addEventListener('scroll', updateActiveLink)
 
 const tl = gsap.timeline()
 
-tl.to('.s1',{
+tl.to('.s1', {
 	scrollTrigger: {
 		trigger: '.who',
-		start: 'top center', 
-        end: '', 
-        scrub: 1,
+		start: 'top center',
+		end: '',
+		scrub: 1,
 	},
-	yPercent:-90,
-	
-}).to('.s2',{
-	scrollTrigger: {
-		trigger: '.steps',
-		start: '-=300px center', 
-        end: '', 
-        scrub: 1,
-	},
-	yPercent:-70,
-	
-}).to('.s3',{
-	scrollTrigger: {
-		trigger: '.steps',
-		start: 'start center', 
-        // end: '+=300', 
-        scrub: 1,
-	},
-	yPercent:-100,
-	
+	yPercent: -90,
 })
-
-
+	.to('.s2', {
+		scrollTrigger: {
+			trigger: '.steps',
+			start: '-=300px center',
+			end: '',
+			scrub: 1,
+		},
+		yPercent: -70,
+	})
+	.to('.s3', {
+		scrollTrigger: {
+			trigger: '.steps',
+			start: 'start center',
+			// end: '+=300',
+			scrub: 1,
+		},
+		yPercent: -100,
+	})
 
 document.querySelectorAll('.card').forEach(card => {
-	const video = card.querySelector('video');
+	const video = card.querySelector('video')
 
 	card.addEventListener('mouseenter', () => {
-	  video.play();
-	});
+		video.play()
+	})
 
 	card.addEventListener('mouseleave', () => {
-	  video.pause();
-	  video.currentTime = 0;
-	});
-  });
+		video.pause()
+		video.currentTime = 0
+	})
+})
 
+const listItems = document.querySelectorAll('.serv-mob__item')
 
-  const listItems = document.querySelectorAll('.serv-mob__item');
-
-  // Добавляем обработчик события на каждый элемент
-  listItems.forEach(item => {
+// Добавляем обработчик события на каждый элемент
+listItems.forEach(item => {
 	item.addEventListener('click', () => {
-	  // Удаляем активный класс у всех элементов
-	  listItems.forEach(i => i.classList.remove('active-tab'));
-	  
-	  // Добавляем активный класс к текущему элементу
-	  item.classList.add('active-tab');
-	});
-  });
+		// Удаляем активный класс у всех элементов
+		listItems.forEach(i => i.classList.remove('active-tab'))
+
+		// Добавляем активный класс к текущему элементу
+		item.classList.add('active-tab')
+	})
+})
 
 // Паралакс
 
+// const parallaxElement = document.getElementById("parallax");
+// const parallaxCont = document.querySelector(".paralax-wraper");
 
-	// const parallaxElement = document.getElementById("parallax");
-	// const parallaxCont = document.querySelector(".paralax-wraper");
+// parallax-container
+// paralax-wraper
 
+// if (parallaxCont) {
+// 	parallaxCont.addEventListener("mousemove", function (e) {
+// 		const x = (window.innerWidth - e.pageX * 2) / 600;
+// 		const y = (window.innerHeight - e.pageY * 2) / 600;
 
-	// parallax-container
-	// paralax-wraper
+// 		parallaxElement.style.transform = `translate(${x}%, ${y}%) scale(1.1)`;
 
+// 	});
 
+// 	parallaxCont.addEventListener("mouseleave", () => {
+// 		parallaxElement.style.transform = "translate(${translateX}%, ${translateY}%) scale(1.1)";
+// 	});
+// }
 
-	// if (parallaxCont) {
-	// 	parallaxCont.addEventListener("mousemove", function (e) {
-	// 		const x = (window.innerWidth - e.pageX * 2) / 600;
-	// 		const y = (window.innerHeight - e.pageY * 2) / 600;
-
-	// 		parallaxElement.style.transform = `translate(${x}%, ${y}%) scale(1.1)`;
-
-	// 	});
-
-	// 	parallaxCont.addEventListener("mouseleave", () => {
-	// 		parallaxElement.style.transform = "translate(${translateX}%, ${translateY}%) scale(1.1)";
-	// 	});
-	// }
-
-
-
-let header = document.querySelector(".hero-second")
-	// headerH = document.querySelector(".header").clientHeight;
+let header = document.querySelector('.hero-second')
+// headerH = document.querySelector(".header").clientHeight;
 
 document.onscroll = function () {
-	let scroll = window.scrollY;
+	let scroll = window.scrollY
 	if (scroll == 0) {
-		header.classList.remove("hero__top-fixed");
-	}if (scroll > 600) {
-		header.classList.add("hero__top-fixed");
-	} else {
-		header.classList.remove("hero__top-fixed");
+		header.classList.remove('hero__top-fixed')
 	}
-};
+	if (scroll > 600) {
+		header.classList.add('hero__top-fixed')
+	} else {
+		header.classList.remove('hero__top-fixed')
+	}
+}
 
-document.getElementById('menu').addEventListener('click', function() {
-	document.getElementById('nav').classList.add('active');
-});
+document.getElementById('menu').addEventListener('click', function () {
+	document.getElementById('nav').classList.add('active')
+})
 
-document.getElementById('menu2').addEventListener('click', function() {
-	document.getElementById('nav').classList.add('active');
-});
+document.getElementById('menu2').addEventListener('click', function () {
+	document.getElementById('nav').classList.add('active')
+})
 
-document.getElementById('close').addEventListener('click', function() {
-	document.getElementById('nav').classList.remove('active');
-});
+document.getElementById('close').addEventListener('click', function () {
+	document.getElementById('nav').classList.remove('active')
+})
 
-const menuItems = document.querySelectorAll('.nav__link');
+const menuItems = document.querySelectorAll('.nav__link')
 
 // Добавляем обработчик событий для каждого элемента меню
-menuItems.forEach(function(item) {
-	item.addEventListener('click', function() {
+menuItems.forEach(function (item) {
+	item.addEventListener('click', function () {
 		// Получаем целевой блок
-		const targetBlock = document.getElementById('nav');
-		
+		const targetBlock = document.getElementById('nav')
+
 		// Добавляем класс к целевому блоку
-		targetBlock.classList.remove('active');
-	});
-});
+		targetBlock.classList.remove('active')
+	})
+})
 
-document.querySelector('.s4').addEventListener('mousemove', function(e) {
-	const element = this;
-	const rect = element.getBoundingClientRect();
-	const offsetX = e.clientX - rect.left - rect.width / 2;
-	const offsetY = e.clientY - rect.top - rect.height / 2;
-  
-	const rotationX = offsetY / 10;
-	const rotationY = -offsetX / 10;
-  
-	element.style.transform = `perspective(500px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
-  });
-  
-  document.querySelector('.s4').addEventListener('mouseleave', function() {
-	this.style.transform = 'perspective(500px) rotateX(0) rotateY(0)';
-  });
+document.querySelector('.s4').addEventListener('mousemove', function (e) {
+	const element = this
+	const rect = element.getBoundingClientRect()
+	const offsetX = e.clientX - rect.left - rect.width / 2
+	const offsetY = e.clientY - rect.top - rect.height / 2
 
+	const rotationX = offsetY / 10
+	const rotationY = -offsetX / 10
+
+	element.style.transform = `perspective(500px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`
+})
+
+document.querySelector('.s4').addEventListener('mouseleave', function () {
+	this.style.transform = 'perspective(500px) rotateX(0) rotateY(0)'
+})
 
 // const accordionSections = document.querySelectorAll('.accordion-section');
 
@@ -248,8 +234,3 @@ document.querySelector('.s4').addEventListener('mousemove', function(e) {
 // fab.addEventListener('click', function(){
 // 	fab.classList.remove('fab-active');
 // });
-
-
-
-
-
